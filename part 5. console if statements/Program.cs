737fd37de0898493e11a55abb7be42b4754d9b6a
Tilson Beattie <BeatTils377@GoTVDSB.ca>
2020-09-29 11:48:38 -0400

    done

--- part 5. console if statements/Program.cs
+++ part 5. console if statements/Program.cs
@@ -10,6 +10,63 @@ namespace part_5.console_if_statements
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter Your Grade!");
+
+            string grade = Console.ReadLine();
+            int condition = Convert.ToInt32(grade);
+
+            if (condition >= 80)
+            {
+
+
+
+                Console.WriteLine("Good Job!");
+
+
+
+            }
+            else if (condition <= 79)
+            {
+                Console.WriteLine("Try for a higher grade!");
+
+            }
+
+
+
+
+
+            Console.WriteLine("Please guess a number bettween 10-20");
+
+            string number = Console.ReadLine();
+            int guess = Convert.ToInt32(number);
+
+            if (guess == 12)
+
+
+            {
+                Console.WriteLine("You guessed it. The number is 12!");
+            }
+            else if (guess == 11)
+            
+            {
+                Console.WriteLine("Your SO Close!");
+            }
+
+            else if (guess == 13)
+            {
+
+                Console.WriteLine("Your SO Close!");
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
 
 
 
